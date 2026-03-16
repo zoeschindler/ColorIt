9d0248e7e41bc87e18a70c5ba9582c84f209878d
zoeschindler <zoe.s@freenet.de>
2026-03-16 11:05:29 +0100

    update

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 ################################################################################
-# PRETTY COLORING BASED ON FURTHEST DISTANCE SAMPLING
+# PRETTY COLORING BASED ON FURTHEST POINT SAMPLING
 ################################################################################
 
 # SETUP ========================================================================
@@ -7,33 +7,96 @@
 # load packages
 library(colorspace)
 
+# HELPERS ======================================================================
+
+get_pal <- function(name = "rainbow") {
+  all_pal <- list(
+    "sky" = c(), # blue to orange
+    "sea" = c(), # blue to green
+    "cozy" = c(), # teal to orange
+    "fairy" = c(), # pink to purple to blue
+    "winter" = c(), # red to blue
+    "rainbow" = c(), # normal rainbow
+    "pastel" = c(), # pastel rainbow
+    "candy" = c() # bright rainbow
+  )
+  return(all_pal[[name]])
+}
+
+dummy_input <- function(n = 20) {
+  out <- c()
+  for (i in 1:n) {
+    curr <- data.frame(
+      "ID" = i,
+      "x" = runif(1, min=-5, max=+5) + rnorm(5),
+      "y" = runif(1, min=-5, max=+5) + rnorm(5),
+      "z" = rnorm(5))
+    out <- rbind(out, curr)
+  }
+  return(out)
+}
+
+distances <- function(mat, cols) {
+  return(NA)
+}
+
 # FUNCTIONS ====================================================================
+
+# dummy inputs
+df = dummy_input()
+col = "sky"
+ncol = 10
 
 # check inputs
 
 # assign color palette
+if (length(col) > 1) {
+  pal <- colorRampPalette(col)
+} else {
+  pal <- get_pal(col)
+}
 
 # prepare output data
+out <- c()
 
-# begin at a random ID
+# calculate ID centers
 
-# assign a random color
+# create lookup -> color ID, color RGB, color LAB
 
-# calculate color distances
+# calculate color distances (matrix)
 
-# calculate spatial distances
+# calculate spatial distances (graphs)
+# -> kdtrees?
+
+# get mean distance of n nearest neighbours per instance
+
+# sort IDs depending on average distance to neighbours
+
+# assign first ID an random color
 
 # loop through all IDs
 
-  # get spatially closest other point
+  # neighbours have no color -> random color
 
-  # get most different color
+  # neighbours have color
+    
+    # check if all colors have been used
+
+    # yes:
+
+    # new, most different color from neighbours
+
+    # no:
+
+      # for each color not yet assigned to a neighbour
+
+        # calculate total color distance
+
+    # get most different color (largest distance)
 
   # assign ID the color
 
-  # update outputs
-
-# alter colors depending on height
+  # assign RGB color to points
 
 # return results
 
