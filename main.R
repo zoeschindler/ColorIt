ceb5f2a5afc21e0db23db9c3655ac457164629b2
zoeschindler <zoe.s@freenet.de>
2026-03-16 11:41:24 +0100

    colors

--- main.R
+++ main.R
@@ -10,15 +10,17 @@ library(colorspace)
 # HELPERS ======================================================================
 
 get_pal <- function(name = "rainbow") {
+  # modified from coolors.co
   all_pal <- list(
-    "sky" = c(), # blue to orange
-    "sea" = c(), # blue to green
-    "cozy" = c(), # teal to orange
-    "fairy" = c(), # pink to purple to blue
-    "winter" = c(), # red to blue
-    "rainbow" = c(), # normal rainbow
-    "pastel" = c(), # pastel rainbow
-    "candy" = c() # bright rainbow
+    "sky" = c("#f2c13a","#ef7239","#f23184","#955ae8","#5c96f5"),
+    "sea" = c("#d9ed92","#99d98c","#52b69a","#168aad","#1e6091","#154366","#0C304C"),
+    "cozy" = c("#41764c","#a7c957","#eadbb3","#d68b8b","#bc4749"),
+    "fairy" = c("#cd92ef","#ffadcb","#fed2e2","#b1e2fc","#9bc1ff"),
+    "winter" = c("#007DA3","#00afb9","#fdfcdc","#fdca9b","#ee6258"),
+    "rainbow" = c("#f52e2e","#f5982e","#f5d42e","#dcf636","#acf636","#36f6a6","#36e9f6","#3d90ee","#7336f6","#c236f6"),
+    "pastel" = c("#fcf7b7","#fed9bb","#ffbfc3","#f7aae7","#c4a6f5","#90bbf8","#7cd9f8","#7beff9","#85f9e0","#a4fdac"),
+    "candy" = c("#9137ff","#ff5ce4","#ff4545","#fee440","#00bbf9","#00f5bc"),
+    "boring" = c("#DEE2E6", "#191C1F")
   )
   return(all_pal[[name]])
 }
@@ -103,6 +105,11 @@ out <- c()
 # EXECUTION ====================================================================
 
 # create dummy data
+df <- dummy_input()
+
+# check data
+ggplot2::ggplot(df) +
+  ggplot2::geom_point(ggplot2::aes(x=x, y=y, col = ID))
 
 # add color values
 
