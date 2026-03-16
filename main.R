6e110e9e51a9ba34866d50ce6d4d433635518957
zoeschindler <zoe.s@freenet.de>
2026-03-16 13:18:47 +0100

    merge with my code

--- main.R
+++ main.R
@@ -13,8 +13,8 @@ library(RANN)
 
 get_pal <- function(name = "rainbow") {
   # modified from coolors.co
-  all_pal <- list(
-    "vibrant_summer" = c("#ff595e","#ff924c","#ffca3a","#c5ca30","#8ac926","#52a675","#1982c4","#4267ac","#6a4c93"), # red to green to blue to purple
+  palettes <- list(
+    "vibrant_summer" = c("#ff595e","#ff924c","#ffca3a","#c5ca30","#8ac926","#52a675","#1982c4","#4267ac","#6a4c93"),
     "sky" = c("#f2c13a","#ef7239","#f23184","#955ae8","#5c96f5"),
     "sea" = c("#d9ed92","#99d98c","#52b69a","#168aad","#1e6091","#154366","#0C304C"),
     "cozy" = c("#41764c","#a7c957","#eadbb3","#d68b8b","#bc4749"),
@@ -25,7 +25,7 @@ get_pal <- function(name = "rainbow") {
     "candy" = c("#9137ff","#ff5ce4","#ff4545","#fee440","#00bbf9","#00f5bc"),
     "boring" = c("#DEE2E6", "#191C1F")
   )
-  return(all_pal[[name]])
+  return(colorRampPalette(palettes[[name]]))
 }
 
 dummy_input <- function(n = 20) {
@@ -41,87 +41,79 @@ dummy_input <- function(n = 20) {
   return(out)
 }
 
-distances <- function(mat, cols) {
-  return(NA)
-}
-
 # FUNCTIONS ====================================================================
 
-# dummy inputs
-df = dummy_input()
-col = "vibrant_summer"
-ncol = 10
-n_neighbors = 8
+color_ids <- function(
+    df = dummy_input(),
+    col = "sky",
+    ncol = 12,
+    n_neighbors = 8) {
 
-# check inputs
+  # check inputs
+  
+  # assign color palette
+  pal_fun <- ifelse(length(col) > 1, colorRampPalette(col), get_pal(col))
+  pal <- pal_fun(ncol)
+  
+  # prepare output data
+  out <- c()
+  
+  # calculate ID centers
+  centers <- df[, .(x = mean(x), y = mean(y), z = mean(z)), by = ID]
+  
+  # create color lookup
+  color_RGB <- colorspace::hex2RGB(pal)
+  color_LAB <- as(color_RGB, "LAB")
+  color_lookup <- data.table::data.table(
+    "ID" = 1:ncol,
+    "color_ID" = pal,
+    "color_RGB" = split(color_RGB@coords, seq(nrow(color_RGB@coords))),
+    "color_LAB" = split(color_LAB@coords, seq(nrow(color_LAB@coords)))
+  )
+  
+  # calculate LAB color distances (matrix)
+  lab_matrix <- do.call(rbind, color_lookup$color_LAB)
+  color_distances <- as.matrix(dist(lab_matrix))
+  
+  # calculate spatial distances (graphs)
+  kdtree <- RANN::nn2(centers[, .(x, y, z)], k = n_neighbors)
+  
+  # get mean distance of n nearest neighbours per instance
+  mean_distances <- rowMeans(kdtree$nn.dists)
+  
+  # sort IDs depending on average distance to neighbours
+  sorted_ids <- centers[order(-mean_distances), ID]
+  
+  # assign first instance ID a random color
+  
+  
+  # loop through all IDs
+  
+    # neighbours have no color -> random color
+  
+    # neighbours have color
+      
+      # check if all colors have been used
+  
+      # yes:
+  
+      # new, most different color from neighbours
+  
+      # no:
+  
+        # for each color not yet assigned to a neighbour
+  
+          # calculate total color distance
+  
+      # get most different color (largest distance)
+  
+    # assign ID the color
+  
+    # assign RGB color to points
+  
+  # return results
 
-# assign color palette
-if (length(col) < ncol) {
-  pal <- colorRampPalette(get_pal(col))
-  pal <- pal(ncol)
-} else {
-  pal <- sample(get_pal(col), ncol)
 }
-
-# prepare output data
-out <- c()
-
-# calculate ID centers
-centers <- df[, .(x = mean(x), y = mean(y), z = mean(z)), by = ID]
-
-# create lookup -> color ID, color RGB, color LAB
-color_RGB = colorspace::hex2RGB(pal)
-color_LAB = as(colorspace::hex2RGB(pal), "LAB")
-
-lookup <- data.table::data.table(
-  "ID" = 1:ncol,
-  "color_ID" = pal,
-  "color_RGB" = split(color_RGB@coords, seq(nrow(color_RGB@coords))),
-  "color_LAB" = split(color_LAB@coords, seq(nrow(color_LAB@coords)))
-)
-
-# calculate LAB color distances (matrix)
-lab_matrix <- do.call(rbind, lookup$color_LAB)
-color_distances <- as.matrix(dist(lab_matrix))
-
-# calculate spatial distances (graphs)
-# -> kdtrees?
-kdtree <- RANN::nn2(centers[, .(x, y, z)], k = n_neighbors)
-
-# get mean distance of n nearest neighbours per instance
-mean_distances <- rowMeans(kdtree$nn.dists)
-
-# sort IDs depending on average distance to neighbours
-sorted_ids <- centers[order(-mean_distances), ID]
-
-# assign first instance ID a random color
-
-
-# loop through all IDs
-
-  # neighbours have no color -> random color
-
-  # neighbours have color
-    
-    # check if all colors have been used
-
-    # yes:
-
-    # new, most different color from neighbours
-
-    # no:
-
-      # for each color not yet assigned to a neighbour
-
-        # calculate total color distance
-
-    # get most different color (largest distance)
-
-  # assign ID the color
-
-  # assign RGB color to points
-
-# return results
 
 # EXECUTION ====================================================================
 
