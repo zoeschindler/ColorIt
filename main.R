9ceeb20e5900e557b3c03d389f982b1498bbca4a
zoeschindler <zoe.s@freenet.de>
2026-03-16 08:58:24 +0100

    pseudo code

---
+++ main.R
@@ -0,0 +1,48 @@
+################################################################################
+# PRETTY COLORING BASED ON FURTHEST DISTANCE SAMPLING
+################################################################################
+
+# SETUP ========================================================================
+
+# load packages
+library(colorspace)
+
+# FUNCTIONS ====================================================================
+
+# check inputs
+
+# assign color palette
+
+# prepare output data
+
+# begin at a random ID
+
+# assign a random color
+
+# calculate color distances
+
+# calculate spatial distances
+
+# loop through all IDs
+
+  # get spatially closest other point
+
+  # get most different color
+
+  # assign ID the color
+
+  # update outputs
+
+# alter colors depending on height
+
+# return results
+
+# EXECUTION ====================================================================
+
+# create dummy data
+
+# add color values
+
+# show results
+
+# ==============================================================================
